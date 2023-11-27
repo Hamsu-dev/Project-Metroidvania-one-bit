51b1d9f08f071a6c38e7a25d81fec9c837cd2d97
Sam Lui <hhu354@aucklanduni.ac.nz>
2023-11-28 01:42:19 +1300

    Basic movement

---
+++ player/player.gd
@@ -0,0 +1,56 @@
+extends CharacterBody2D
+
+@export var acceleration = 512
+@export var max_velocity = 64
+@export var friction = 256
+@export var gravity = 200
+@export var jump_force = 128
+@export var max_fall_speed = 120
+
+@onready var animation_player = $AnimationPlayer
+@onready var sprite_2d = $Sprite2D
+
+func _physics_process(delta):
+	var input_axis = Input.get_axis("ui_left", "ui_right")
+	apply_gravity(delta)
+	if is_moving(input_axis):
+		apply_acceleration(delta, input_axis)
+	else:
+		apply_friction(delta)
+	jump_check()
+	update_animations(input_axis)
+	move_and_slide()
+
+
+
+func is_moving(input_axis):
+	return input_axis != 0
+
+
+func apply_gravity(delta):
+	if not is_on_floor():
+		velocity.y = move_toward(velocity.y, max_fall_speed, gravity * delta)
+
+
+func apply_acceleration(delta, input_axis):
+	velocity.x = move_toward(velocity.x, input_axis * max_velocity, acceleration * delta)
+
+
+func apply_friction(delta):
+	velocity.x = move_toward(velocity.x, 0, friction * delta)
+
+
+func jump_check():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = -jump_force
+
+
+func update_animations(input_axis):
+	if is_moving(input_axis):
+		animation_player.play("run")
+		sprite_2d.scale.x = sign(input_axis)
+	else:
+		animation_player.play("idle")
+	
+	if not is_on_floor():
+		animation_player.play("jump")
