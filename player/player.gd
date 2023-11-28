9c89adb7b80a4c32c9c55420e25dd629a0948139
Sam Lui <hhu354@aucklanduni.ac.nz>
2023-11-28 14:36:56 +1300

    Animated Particles & Coyote Jump

--- player/player.gd
+++ player/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+const DustEffectScene = preload("res://effects/dust_effect.tscn")
+
 @export var acceleration = 512
 @export var max_velocity = 64
 @export var friction = 256
@@ -9,18 +11,33 @@ extends CharacterBody2D
 
 @onready var animation_player = $AnimationPlayer
 @onready var sprite_2d = $Sprite2D
+@onready var coyote_jump_timer = $CoyoteJumpTimer
+
 
 func _physics_process(delta):
 	var input_axis = Input.get_axis("ui_left", "ui_right")
 	apply_gravity(delta)
+	# Moving
 	if is_moving(input_axis):
 		apply_acceleration(delta, input_axis)
 	else:
 		apply_friction(delta)
+
 	jump_check()
 	update_animations(input_axis)
+	
+	var was_on_floor = is_on_floor()
 	move_and_slide()
+	var just_left_edge = was_on_floor and not is_on_floor() and velocity.y >= 0
+	if just_left_edge:
+		coyote_jump_timer.start()
 
+
+func create_dust_effect():
+	var dust_effect = DustEffectScene.instantiate()
+	var world = get_tree().current_scene
+	world.add_child(dust_effect)
+	dust_effect.global_position = global_position
 
 
 func is_moving(input_axis):
@@ -41,9 +58,12 @@ func apply_friction(delta):
 
 
 func jump_check():
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = -jump_force
-
+	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
+		if Input.is_action_just_pressed("ui_up"):
+			velocity.y = -jump_force
+	if not is_on_floor():
+		if Input.is_action_just_released("ui_up") and velocity.y < -jump_force / 2:
+			velocity.y = -jump_force / 2
 
 func update_animations(input_axis):
 	if is_moving(input_axis):
