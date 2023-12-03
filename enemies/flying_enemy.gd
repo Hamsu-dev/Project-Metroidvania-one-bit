75fdbee168d793501a068446db9f653e46b397bc
Sam Lui <hhu354@aucklanduni.ac.nz>
2023-12-03 22:19:28 +1300

    Smart Waypoint system for enemies

--- enemies/flying_enemy.gd
+++ enemies/flying_enemy.gd
@@ -2,18 +2,20 @@ extends CharacterBody2D
 
 const EnemyDeathEffectScene = preload("res://effects/enemy_death_effect.tscn")
 
-@onready var animated_sprite_2d = $AnimatedSprite2D
-@onready var stats = $Stats as Stats
-
 @export var acceleration = 100
 @export var max_speed = 40
 
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var stats = $Stats
+@onready var way_point_path_finding = $WayPointPathFinding
+
+func _ready():
+	set_physics_process(false)
 
 func _physics_process(delta):
 	var player = MainInstances.player
 	if player is CharacterBody2D:
-		move_toward_position(player.global_position, delta)
-
+		move_toward_position(way_point_path_finding.pathfinding_next_position, delta)
 
 func move_toward_position(target_position, delta):
 	var direction = global_position.direction_to(target_position)
@@ -21,11 +23,14 @@ func move_toward_position(target_position, delta):
 	animated_sprite_2d.flip_h = global_position < target_position
 	move_and_slide()
 
-
 func _on_hurtbox_hurt(hitbox, damage):
 	stats.health -= damage
 
 
 func _on_stats_no_health():
-	Utils.instantiate_scene_on_world(EnemyDeathEffectScene, global_position)
 	queue_free()
+	Utils.instantiate_scene_on_world(EnemyDeathEffectScene, global_position)
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered():
+	set_physics_process(true)
