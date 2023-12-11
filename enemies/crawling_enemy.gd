9dc7291f963dc5975a99fe3672905fc1a192b205
Sam Lui <hhu354@aucklanduni.ac.nz>
2023-12-11 13:41:53 +1300

    Crawling enemy falling state

--- enemies/crawling_enemy.gd
+++ enemies/crawling_enemy.gd
@@ -6,16 +6,18 @@ enum DIRECTION {LEFT = -1, RIGHT = 1}
 
 @export var crawling_direction = DIRECTION.RIGHT
 @export var max_speed = 150
+@export var spin_speed = 600
 
 @onready var floor_cast = $FloorCast
 @onready var wall_cast = $WallCast
 @onready var stats = $Stats as Stats
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
-
+var gravity = 100
 var is_knockback_active = false
 var knockback_velocity = Vector2()
 var knockback_timer = Timer.new()
-
+var state = crawling_state
 
 signal enemy_died
 
@@ -33,23 +35,37 @@ func initialize_enemy():
 	wall_cast.target_position.x *= crawling_direction
 
 func _physics_process(delta):
-	if is_knockback_active:
-		var knockback_movement = knockback_velocity * delta
-		global_position += knockback_movement
+	state.call(delta)
+	
+	
+func crawling_state(delta):
+	animated_sprite_2d.play("crawl")
+	if wall_cast.is_colliding():
+		global_position = wall_cast.get_collision_point()
+		var wall_normal = wall_cast.get_collision_normal()
+		rotation = wall_normal.rotated(deg_to_rad(90)).angle()
 	else:
-		# Normal movement logic
-		if wall_cast.is_colliding():
-			global_position = wall_cast.get_collision_point()
-			var wall_normal = wall_cast.get_collision_normal()
-			rotation = wall_normal.rotated(deg_to_rad(90)).angle()
-		else:
-			floor_cast.rotation_degrees = -max_speed * crawling_direction * delta
-			if floor_cast.is_colliding():
-				global_position = floor_cast.get_collision_point()
-				var floor_normal = floor_cast.get_collision_normal()
-				rotation = floor_normal.rotated(deg_to_rad(90)).angle()
-			else:
-				rotation_degrees += crawling_direction
+		floor_cast.rotation_degrees = -max_speed * crawling_direction * delta
+		var while_limit = 30
+		while not floor_cast.is_colliding():
+			rotation_degrees += crawling_direction
+			floor_cast.force_raycast_update()
+			while_limit -= 1
+			if while_limit <= 0:
+				state = falling_state
+				break
+			
+		if floor_cast.is_colliding():
+			global_position = floor_cast.get_collision_point()
+			var floor_normal = floor_cast.get_collision_normal()
+			rotation = floor_normal.rotated(deg_to_rad(90)).angle()
+
+func falling_state(delta):
+	animated_sprite_2d.play("fall")
+	rotation_degrees += crawling_direction * spin_speed * delta
+	position.y += gravity * delta
+	if floor_cast.is_colliding() or wall_cast.is_colliding():
+		state = crawling_state
 
 
 func _on_hurtbox_hurt(hitbox, damage, direction):
