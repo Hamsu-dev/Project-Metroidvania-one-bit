35d176a907f3f2061256fa25910a6c877379fdf2
Sam Lui <hhu354@aucklanduni.ac.nz>
2023-12-12 22:29:28 +1300

    World Stash

--- player/powerup.gd
+++ player/powerup.gd
@@ -2,8 +2,16 @@ class_name Powerup
 extends Area2D
 
 
-func pickup():
+func _ready():
+	await get_tree().process_frame
+	var id = WorldStash.get_id(self)
+	var freed = WorldStash.retrieve(id, "freed")
+	if freed: queue_free()
 
+
+func pickup():
+	var id = WorldStash.get_id(self)
+	WorldStash.stash(id, "freed", true)
 	queue_free()
 
 
--- world/world.gd
+++ world/world.gd
@@ -6,17 +6,26 @@ func _ready():
 	RenderingServer.set_default_clear_color(Color.BLACK)
 	Events.door_entered.connect(change_levels)
 	
+func clear_bullets():
+	var bullets = get_tree().get_nodes_in_group("EnemyBullets")
+	for bullet in bullets:
+		bullet.queue_free()
+
 
 func change_levels(door : Door):
 	var player = MainInstances.player as Player
 	if not player is Player: return
+	
+	clear_bullets()
+	
 	level.queue_free()
 	var new_level = load(door.new_level_path).instantiate()
 	add_child(new_level)
 	level = new_level
+	
 	var doors = get_tree().get_nodes_in_group("doors")
 	for found_door in doors:
-		if found_door == door: continue
-		if found_door.connection != door.connection: continue
-		var yoffset = player.global_position.y - door.global_position.y
-		player.global_position = found_door.global_position + Vector2(0, yoffset)
+		if found_door.connection == door.connection and found_door != door:
+			var yoffset = player.global_position.y - door.global_position.y
+			player.global_position = found_door.global_position + Vector2(0, yoffset)
+			break
